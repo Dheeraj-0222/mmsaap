699ae09be6d6ac8e0b01fb8de7a3b0f57d1afc96
Dheeraj <dheeraj22lpu@gmail.com>
2024-12-12 12:32:09 +0530

    test 1

---
+++ src/main/java/com/mmsaap/MmsaapApplication.java
@@ -0,0 +1,13 @@
+package com.mmsaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsaapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmsaapApplication.class, args);
+	}
+
+}
