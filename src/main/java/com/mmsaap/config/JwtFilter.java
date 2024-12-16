ad1b9554468654d26856761c7777e49cdb531d4f
Dheeraj <dheeraj22lpu@gmail.com>
2024-12-16 11:26:06 +0530

    test 4

--- src/main/java/com/mmsaap/config/JwtFilter.java
+++ src/main/java/com/mmsaap/config/JwtFilter.java
@@ -1,20 +1,51 @@
 package com.mmsaap.config;
 
+import com.mmsaap.entity.User;
+import com.mmsaap.repository.UserRepository;
+import com.mmsaap.service.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+    private JWTService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtFilter(JWTService jwtService,
+                     UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader("Authorization");
-        String jwtToken = token.substring(7);
-        System.out.println(jwtToken);
+
+        if(token != null&&token.startsWith("Bearer ")) {
+            String jwtToken = token.substring(8,token.length() - 1);
+            System.out.println(jwtToken);
+            String username = jwtService.getUsername(jwtToken);
+            Optional<User> opUsername = userRepository.findByUsername(username);
+            if (opUsername.isPresent()) {
+                User user = opUsername.get();
+                UsernamePasswordAuthenticationToken
+                        userToken =
+                        new UsernamePasswordAuthenticationToken(user.getUsername(), null, null);
+                userToken.setDetails(new WebAuthenticationDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(userToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
--- src/main/java/com/mmsaap/config/SequrityConfig.java
+++ src/main/java/com/mmsaap/config/SequrityConfig.java
@@ -1,12 +1,20 @@
 package com.mmsaap.config;
 
+import com.mmsaap.service.JWTService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SequrityConfig {
+    private JwtFilter jwtFilter;
+
+    public SequrityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain
@@ -15,6 +23,8 @@ public class SequrityConfig {
     {
         //h(cd)2
         http.csrf().disable().cors().disable();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         //haap
         http.authorizeHttpRequests().anyRequest().permitAll();
         return http.build();
